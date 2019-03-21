ea0c589f1339e8785c9086937d74c1c60a8bfbf4
zjj <760612141@qq.com>
2019-03-21 17:04:54 +0800

    caculate

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h2>calculate</h2>
+    <sum-function :number1="number1" :number2="number2" v-on:getSumFromChild="receiveChildSum"></sum-function>
+    
+    <p>从子组件获取到的值：{{sumFromChild}}</p>
+  </div>
+</template>
+
+<script>
+import sumFunction from '../src/myPlugin/sumFunction/sum-function'; // 引入子组件
+export default {
+  name: 'app',
+  data () {
+    return {
+      number1: 4,
+      number2: 5,
+      sumFromChild:0,
+    }
+  },
+  components:{ //注册插件
+    sumFunction
+  },
+  methods:{
+    receiveChildSum(sum){ //自定义事件，接收子组件的和
+      this.sumFromChild = sum;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/myPlugin/sumFunction/sum-function.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class = 'calulate'>
+      <p>{{number1}} + {{number1}} = {{sum}}</p> 
+      <div>
+          <input class = 'border' type='text' v-model='number1' @blur="sumFunction">
+          <span>+</span>
+          <input class = 'border' type='text' v-model='number2' @blur="sumFunction">
+          <span @click='sumFunction()'>=</span>
+          <span>{{ sum }}</span>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['number1', 'number2'],
+    name: 'sumFunction',
+    data() {
+        return{
+            number1: this.number1?this.number1: 0,
+            number2: this.number2?this.number2: 0,
+            sum: 0
+        }
+    },
+    mouthed() {
+        this.sumFunction();
+    },
+    method: {
+        sumFunction(){
+            let a = Number(this.number1);
+            let b = Number(this.number2);
+            if(isNaN(a) || idNaN(b)){
+                return;
+            }else{
+                this.sum = a + b;
+                this.$emit("getSumFromChild",this.sum);
+            }
+        }
+    }
+}
+</script>
+<style>
+.calculate{
+  width: 100%;
+  line-height: 26px;
+}
+.border {
+    width: 30px;
+    text-align: center;
+}
+</style>
